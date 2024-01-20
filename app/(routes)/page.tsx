d0dfdbfce612262c8be4ab9903fbbd9563904691
qie5814 <qiehidayat@gmail.com>
2024-01-20 09:35:39 +0700

    Environment Setup & Featured Products

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,25 @@
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import Billboard from "@/components/billboard";
+import Container from "@/components/ui/container";
+import ProductList from "@/components/product-list";
+
+export const revalidate = 0;
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("9bf3dd79-d5d3-4d6f-97cf-a4c93bdc49ad");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default HomePage;
